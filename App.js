f4040fde1d050b434bdea842ca4ad0d6743c98fe
Jaison <jaisonschmidt@gmail.com>
2024-04-03 20:12:27 -0300

    Anotações com o que aprendemos

--- App.js
+++ App.js
@@ -7,6 +7,7 @@ import { Button, StyleSheet, Text, View } from 'react-native';
  * ciclo de vida de componentes (useEffect)
  * promises com async / await
  * fetch API
+ * eventos da aplicação (onPress)
  */
 
 export default function App() {
